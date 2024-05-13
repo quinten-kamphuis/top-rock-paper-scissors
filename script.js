c1ed2671b845763c9f7b431d4ef8d5e2e0841c7c
Quinten Kamphuis <163667554+quinten-kamphuis@users.noreply.github.com>
2024-05-13 14:14:20 +0200

    Add animation effect and localstorage scorekeeping

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const bod = document.querySelector('body');
+const originalBackgroundColor = window.getComputedStyle(document.body).backgroundColor;
+
 const gamePrompt = document.querySelector('#game-prompt');
 const buttonElements = document.querySelectorAll('.button');
 const scoreElements = document.querySelectorAll('.score');
@@ -23,18 +26,39 @@ const getComputerChoice = () => {
     }
 };
 
-const updatePrompt = computerSelection => {
-    switch(computerSelection){
-        case 'rock':
-            score.wins++;
+const updatePrompt = (winner, computerSelection) => {
+    let winnerPrompt;
+    switch(winner){
+        case 'player':
+            winnerPrompt = 'You Won!';
             break;
-        case 'paper':
-            score.ties++;
+        case 'equal':
+            winnerPrompt = "It's a tie!";
             break;
-        case 'scissors':
-            score.losses++;
+        case 'computer':
+            winnerPrompt = 'You Lost!';
             break;
     }
+    gamePrompt.innerText = `${winnerPrompt} 
+    The computer chose: ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+};
+
+const saveScoreToLocalStorage = () => {
+    localStorage.setItem('gameScore', JSON.stringify(score));
+};
+
+const loadScoreFromLocalStorage = () => {
+    const scoreData = JSON.parse(localStorage.getItem('gameScore'));
+    if (scoreData) {
+        score.wins = scoreData.wins;
+        score.ties = scoreData.ties;
+        score.losses = scoreData.losses;
+    } else {
+        score.wins = 0;
+        score.ties = 0;
+        score.losses = 0;
+    }
+    updateScore();
 };
 
 const updateScoreBoard = () => {
@@ -59,28 +83,71 @@ const updateScore = winner => {
     updateScoreBoard();
 };
 
+const disableButtons = () => {
+    buttonElements.forEach(elem => {elem.classList.add("disabled");})
+    bod.style.cursor = 'not-allowed';
+    setTimeout(() => {
+        buttonElements.forEach(elem => {elem.classList.remove("disabled");})
+        bod.style.cursor = '';
+    }, 1000);
+};
+
+const backgroundAnimation = winner => {
+    let color;
+
+    switch(winner){
+        case 'player':
+            color = '#2ecc71'
+            break;
+        case 'equal':
+            color = '#bdc3c7'
+            break;
+        case 'computer':
+            color = '#e74c3c'
+            break;
+    }
+
+    bod.style.transition = 'none'; 
+    bod.style.backgroundColor = color; 
+
+
+    setTimeout(() => {
+        bod.style.transition = 'background-color 1.5s ease'; 
+        bod.style.backgroundColor = originalBackgroundColor; 
+    }, 500);
+};
+
 //function playRound(playerSelection, computerSelection) should return wether player won
 const playRound = (playerSelection, computerSelection) => {
+    let winner;
     if (playerSelection === computerSelection){
-        updateScore('equal');
+        winner = 'equal';
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        updateScore('computer');
+        winner = 'computer';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        updateScore('player');
+        winner = 'player';
     } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-        updateScore('computer');
+        winner = 'computer';
     } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        updateScore('player');
+        winner = 'player';
     } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-        updateScore('computer');
+        winner = 'computer';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        updateScore('player');
+        winner = 'player';
     } else {
         console.warn('Something went wrong!');
     }
-    updatePrompt(computerSelection);
+    updateScore(winner);
+    backgroundAnimation(winner);
+    disableButtons();
+    updatePrompt(winner, computerSelection);
+    saveScoreToLocalStorage();
 }
 
 buttonElements.forEach(elem => elem.addEventListener('click', e => {
     playRound(elem.getAttribute('id'), getComputerChoice());
 }));
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadScoreFromLocalStorage();
+});
